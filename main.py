745a30d0c7906ad9ba25cd1dcba037b08c52bc8b
Jesus Gollonet <jesusgollonet@gmail.com>
2024-09-11 09:50:32 +0200

    Read and display images from memory

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests
 import os
 from bs4 import BeautifulSoup
 
+import cv2 as cv
+import numpy as np
+
 response = requests.get(
     "https://www.aemet.es/es/eltiempo/prediccion/maritima?opc1=0&opc3=0&area=and2"
 )
@@ -16,6 +19,25 @@ for image in images:
     try:
         response = requests.get(f"https://www.aemet.es/{image['data-src']}")
         response.raise_for_status()
+
+        # Convert the response content to a NumPy array for OpenCV
+        image_data = np.frombuffer(response.content, np.uint8)
+
+        # Decode the image directly from the byte array
+        img = cv.imdecode(image_data, cv.IMREAD_COLOR)
+        img = img[22:500, 50:760]
+
+        # Run your color detection or any OpenCV operation on `img`
+        cv.imshow("Image", img)
+
+        # Wait for a key press
+        key = cv.waitKey(0)
+
+        # If 'q' is pressed, break the loop and exit the program
+        if key == ord("q"):
+            print("Exiting the program.")
+            break
+        cv.destroyAllWindows()
     except Exception as e:
         print(f"Error downloading image: {e}")
 
