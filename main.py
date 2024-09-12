e58cde59fc0659cd0aa0bf2334d21030947898f0
Jesus Gollonet <jesusgollonet@gmail.com>
2024-09-12 13:12:45 +0200

    Display time and only if there are waves

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import requests
 import os
 from bs4 import BeautifulSoup
+import humanize
+from datetime import datetime, timedelta
 
 import cv2 as cv
 import numpy as np
+
+MIN_AREA = 700
 
 response = requests.get(
     "https://www.aemet.es/es/eltiempo/prediccion/maritima?opc1=0&opc3=0&area=and2"
@@ -23,20 +27,32 @@ def hsv_bounds(hsv: np.ndarray, margin: int = 2) -> tuple:
     return (np.array([h - margin, 100, 100]), np.array([h + margin, 255, 255]))
 
 
-# I don't really need to see the image, i can find contours in the mask
-
-
 def detect_yellow():
     return None
 
 
+def human_date(date):
+    return humanize.naturaltime(date)
+
+
 # Get all images with data-src attribute
 images = soup.find_all("img", {"data-src": True})
+
+
+def date_from_image(image):
+    date_source = image["data-src"].split("/")[-1].split("_")[0]
+    base_date_str, offset = date_source.split("+")
+    base_date = datetime.strptime(base_date_str, "%Y%m%d%H")
+    date = base_date + timedelta(hours=int(offset))
+    return date
+
+
 for image in images:
     # Download image
     try:
         response = requests.get(f"https://www.aemet.es/{image['data-src']}")
         response.raise_for_status()
+        d = date_from_image(image)
 
         # Convert the response content to a NumPy array for OpenCV
         image_data = np.frombuffer(response.content, np.uint8)
@@ -55,27 +71,28 @@ for image in images:
 
         areas = [cv.contourArea(contour) for contour in contours]
         total_area = sum(areas)
-        cv.putText(
-            img,
-            str(total_area),
-            (10, 30),
-            cv.FONT_HERSHEY_SIMPLEX,
-            1,
-            (255, 255, 255),
-            2,
-        )
-        # area = cv.contourArea(contours[0])
-        # Run your color detection or any OpenCV operation on `img`
-        cv.imshow("Image", img)
+        if total_area > MIN_AREA:
+            cv.putText(
+                img,
+                str(human_date(d) + ": " + str(total_area)),
+                (10, 30),
+                cv.FONT_HERSHEY_SIMPLEX,
+                1,
+                (255, 255, 255),
+                2,
+            )
+            # area = cv.contourArea(contours[0])
+            # Run your color detection or any OpenCV operation on `img`
+            cv.imshow("Image", img)
 
-        # Wait for a key press
-        key = cv.waitKey(0)
+            # Wait for a key press
+            key = cv.waitKey(0)
 
-        # If 'q' is pressed, break the loop and exit the program
-        if key == ord("q"):
-            print("Exiting the program.")
-            break
-        cv.destroyAllWindows()
+            # If 'q' is pressed, break the loop and exit the program
+            if key == ord("q"):
+                print("Exiting the program.")
+                break
+            cv.destroyAllWindows()
     except Exception as e:
         print(f"Error downloading image: {e}")
 
