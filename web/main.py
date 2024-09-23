07e0942ec9dfe04ea88591e469077a8636805f21
Jesus Gollonet <jesusgollonet@gmail.com>
2024-09-23 13:23:26 +0200

    Web dockerfile deploy

--- web/main.py
+++ web/main.py
@@ -44,7 +44,6 @@ def get():
 
 
 if __name__ == "__main__":
-    serve()
-    # import uvicorn
+    import uvicorn
 
-    # uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
